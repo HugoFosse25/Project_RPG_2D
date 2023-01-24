1757581bc7f0288aec6ead5001b0368708b34668
Hugo Fossé <fosse.hugo25@gmail.com>
2023-01-24 17:03:16 +0100

    Gérer Animations
    Change l'animation en fonction de la direction

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,10 +10,13 @@ public class PlayerMovement : MonoBehaviour
     private InputAction moveAction;
     private Vector2 velocity = Vector2.zero;
     [SerializeField] private float speed = 5f;
+    private Animator anim;
+    private int direction = 0;
     void Start()
     {
         manager = GameManager.GetInstance();
         inputs = manager.GetInputs();
+        anim = GetComponent<Animator>();
 
         moveAction = inputs.actions.FindAction("Move");     //Récupère l'Action de déplacement présent dans l'input action du GameManager
     }
@@ -22,9 +25,35 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         Vector2 _moveValue = moveAction.ReadValue<Vector2>();   //Récupère la direction sur laquelle le joueur veut aller
+        direction = SetDirection(_moveValue);
 
         velocity = _moveValue * speed;
 
         transform.position += new Vector3(velocity.x * Time.fixedDeltaTime, velocity.y * Time.fixedDeltaTime, 0); //Transforme mètres par frame en mètres par seconde et applique la force au joueur
+
+        anim.SetInteger("direction", direction); 
+    }
+
+    private int SetDirection(Vector2 _vector)   //Renvoie un nombre suivant la direction du joueur servant a jouer la bonne animation
+    {
+        if(_vector.x > 0)   //Si le joueur vas a droite
+        {
+            return 6;
+        }
+        else if(_vector.x < 0)  //Si le joueur vas a gauche
+        {
+            return 4;
+        }
+
+        if(_vector.y > 0)   //Si le joueur vas en haut
+        {
+            return 8;
+        }
+        else if(_vector.y < 0)  //Si le joueur vas en bas
+        {
+            return 2;
+        }
+
+        return 0;
     }
 }
